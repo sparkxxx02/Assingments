f0d4ccef68c761fef5289df51731062e440e0724
sparky02-Github <ujjwalshrivastava00@gmail.com>
2022-01-22 16:24:37 +0530

    First commit from milestone2

---
+++ Milestone2/src/main/java/com/assingment/Milestone2/Milestone2Application.java
@@ -0,0 +1,13 @@
+package com.assingment.Milestone2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milestone2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Milestone2Application.class, args);
+	}
+
+}
---
+++ Milestone2/src/test/java/com/assingment/Milestone2/Milestone2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assingment.Milestone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Milestone2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
